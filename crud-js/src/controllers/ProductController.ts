f9f4a8e8def8f18b1e088ed2fe30bafa2ce09ac2
unknown <kevinlima138@gmail.com>
2023-05-16 23:30:24 -0300

    Subindo alterações de routes

--- crud-js/src/controllers/ProductController.ts
+++ crud-js/src/controllers/ProductController.ts
@@ -12,10 +12,29 @@ class ProductController {
         return await Product.find()
     }
 
+    async readById(productId: string)
+    {
+        await connectMongo();
+        return await Product.findOne({_id:productId}); //busca um registro do produto
+    }
+
     async create(product: any) {
         await connectMongo();
-        return Product.create(product)
+        return await Product.create(product)
     }
+
+    async deleteOne(productId: string)
+    {
+        await connectMongo()
+        return await Product.findOneAndDelete({_id:productId})
+    }
+
+    async updateProduct(productId: string, body: any)
+    {
+        await connectMongo();
+        return await Product.findOneAndUpdate({_id:productId},{...body}, {new: true}); //função encontra e retorna o registro atualizado.
+    }
+
 }
 
 export default new ProductController();
